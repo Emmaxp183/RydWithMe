b6d6858ef70fe4733df498cb5eb799809870d107
Emmanuel Mensah <immaxp183@gmail.com>
2022-12-05 13:50:12 +0100

    Initial Commit

---
+++ RydWithMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RydWithMe
+//
+//  Created by macbook on 05/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
